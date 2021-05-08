57dd49b22eb298ff7fe13d38f2ce81fc055d3100
Jacob Ho <clh161@gmail.com>
2021-05-09 00:56:00 +0100

    make xml

--- __test__/Generator.test.js
+++ __test__/Generator.test.js
@@ -1,11 +1,12 @@
 import Generator from '../lib/Generator';
-import Router from './router/Router1';
+import Router from './data/Router1';
+import fs from 'fs';
 
 it('Print xml', () => {
   const generator = new Generator(
     'https://react-router-sitemap-generator.com',
     Router
   );
-  const xml = generator.getXML();
-  expect(xml).toEqual(['/generator', '/sitemap', '/router', '/react', '/']);
+  const sitemap = fs.readFileSync('./__test__/data/sitemap1.xml').toString();
+  expect(generator.getXML()).toEqual(sitemap);
 });
