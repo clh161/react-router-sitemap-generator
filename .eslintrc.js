3ad99e0412dd3ded202d2382e64a316fdbcb1652
Jacob Ho <clh161@gmail.com>
2021-05-08 23:55:12 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-underscore-dangle': ['off'],
+    'react/jsx-filename-extension': ['off'],
+  },
+};
--- __test__/router/Router1.js
+++ __test__/router/Router1.js
@@ -1,5 +1,6 @@
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Switch } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
 
 export default function Router() {
